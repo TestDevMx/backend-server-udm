9d3d2e5e77dd772c70d203e50024c4721927a340
testdevmx <test.dev.mx@gmail.com>
2019-03-18 02:32:07 -0600

    Usuario y Token listo

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 
-
 //* Requires
 var express = require('express');
 var mongoose = require("mongoose");
+var bodyParser = require('body-parser');
 
 
 //* Inicializar variables
 var app = express();
+
+//* Body parser
+// ! parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//* Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 
 
@@ -19,15 +29,10 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', ( err, res )
 
 
 
-//* Rutas
-app.get('/', (req, res, next) => {
-
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Petición realizada correctamente'
-    });
-
-});
+//* Rutas (midelware)
+app.use('/login', loginRoutes);
+app.use('/usuario', usuarioRoutes);
+app.use('/', appRoutes);
 
 
 //* Escuchar peticiones
